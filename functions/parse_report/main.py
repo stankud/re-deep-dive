47fda2a4e48ed5a3c9110168bb604dea016209f8
Stan Kudrow <st4sik@gmail.com>
2018-11-12 09:33:09 -0500

    add parse report function

---
+++ functions/parse_report/main.py
@@ -0,0 +1,27 @@
+"""
+Lambda example with external dependency
+"""
+import sys
+sys.path.insert(0, "./lib")
+import logging
+import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handle(event, context):
+    """
+    Lambda handler
+    """
+    logger.info("%s - %s", event, context)
+
+    url = "https://api.ipify.org?format=json"
+
+    raw = requests.get(url)
+    logger.info("%s", raw)
+    result = raw.json()
+
+    logger.info("Lambda IP: %s", result['ip'])
+
+    return event
