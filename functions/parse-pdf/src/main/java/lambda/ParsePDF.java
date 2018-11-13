d2c81ce0bb5051c72c9e71f651be518c146f35b9
Stan Kudrow <st4sik@gmail.com>
2018-11-13 18:19:38 -0500

    add s3 to java lambda

--- functions/parse-pdf/src/main/java/lambda/ParsePDF.java
+++ functions/parse-pdf/src/main/java/lambda/ParsePDF.java
@@ -3,23 +3,39 @@ package lambda;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import java.io.FileOutputStream;
+import java.io.File;
 
 
 public class ParsePDF {
 
     public static class ParsePDFRequest {
-        String filePath;
+        String bucketName;
+        String keyName;
 
-        public String getFilePath() {
-            return filePath;
+        public String getBucketName() {
+            return bucketName;
         }
 
-        public void setFilePath(String filePath) {
-            this.filePath = filePath;
+        public String getKeyName() {
+            return keyName;
         }
 
-        public ParsePDFRequest(String filePath) {
-            this.filePath = filePath;
+        public void setBucketName(String bucketName) {
+            this.bucketName = bucketName;
+        }
+
+        public void setKeyName(String keyName) {
+            this.keyName = keyName;
+        }
+
+        public ParsePDFRequest(String bucketName, String keyName) {
+            this.bucketName = bucketName;
+            this.keyName = keyName;
         }
 
         public ParsePDFRequest() {
@@ -46,17 +62,26 @@ public class ParsePDF {
     }
 
     public ParsePDFResponse handler(ParsePDFRequest event, Context context) {
+        final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+
         String jarpath = "lib/tabula-1.0.3-jar-with-dependencies.jar";
-        String filePath = event.getFilePath();
-        String lclFilePath = "myLocalPath.pdf";
-        String comm = String.format("java -jar %s -r %s", jarpath, lclFilePath);
+        String srcBucketName = event.getBucketName();
+        String srcKeyName = event.getKeyName();
+        String srcLclFilePath = "/tmp/input.pdf";
+        String dstLclFilePath = "/tmp/output.csv";
+        String comm = String.format(
+          "java -jar %s -o %s -r %s", jarpath, dstLclFilePath, srcLclFilePath);
         String s;
         Process p;
 
-        System.out.println(filePath);
+        System.out.println(srcKeyName);
         System.out.println(comm);
         try {
-            p = Runtime.getRuntime().exec("ls -lah");
+            S3Object o = s3.getObject(srcBucketName, srcKeyName);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            FileOutputStream fos = new FileOutputStream(new File(srcLclFilePath));
+
+            p = Runtime.getRuntime().exec("ls -lah /tmp");
             BufferedReader br = new BufferedReader(
                 new InputStreamReader(p.getInputStream()));
 
@@ -67,6 +92,6 @@ public class ParsePDF {
             System.out.println ("exit: " + p.exitValue());
             p.destroy();
         } catch (Exception e) {}
-        return new ParsePDFResponse(event.getFilePath());
+        return new ParsePDFResponse(event.getKeyName());
     }
 }
