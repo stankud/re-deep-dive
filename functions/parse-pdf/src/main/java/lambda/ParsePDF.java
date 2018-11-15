1886286971dd12a7e90f652046866ed9d97c8340
Stan Kudrow <stan.kudrow@vice.com>
2018-11-15 18:02:59 -0500

    upload csv back to s3

--- functions/parse-pdf/src/main/java/lambda/ParsePDF.java
+++ functions/parse-pdf/src/main/java/lambda/ParsePDF.java
@@ -2,45 +2,64 @@ package lambda;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.File;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 
 public class ParsePDF {
 
-    public static class ParsePDFRequest {
-        String bucketName;
-        String keyName;
+  public static class ParsePDFRequest {
+      String srcBucketName, srcKeyName, dstBucketName, dstKeyName;
 
-        public String getBucketName() {
-            return bucketName;
-        }
+      public String getSrcBucketName() {
+          return srcBucketName;
+      }
 
-        public String getKeyName() {
-            return keyName;
-        }
+      public String getSrcKeyName() {
+          return srcKeyName;
+      }
 
-        public void setBucketName(String bucketName) {
-            this.bucketName = bucketName;
-        }
+      public void setSrcBucketName(String srcBucketName) {
+          this.srcBucketName = srcBucketName;
+      }
 
-        public void setKeyName(String keyName) {
-            this.keyName = keyName;
-        }
+      public void setSrcKeyName(String srcKeyName) {
+          this.srcKeyName = srcKeyName;
+      }
 
-        public ParsePDFRequest(String bucketName, String keyName) {
-            this.bucketName = bucketName;
-            this.keyName = keyName;
-        }
+      public String getDstBucketName() {
+          return dstBucketName;
+      }
 
-        public ParsePDFRequest() {
-        }
-    }
+      public String getDstKeyName() {
+          return dstKeyName;
+      }
 
+      public void setDstBucketName(String dstBucketName) {
+          this.dstBucketName = dstBucketName;
+      }
+
+      public void setDstKeyName(String dstKeyName) {
+          this.dstKeyName = dstKeyName;
+      }
+
+      public ParsePDFRequest(String srcBucketName, String srcKeyName, String dstBucketName, String dstKeyName) {
+          this.srcBucketName = srcBucketName;
+          this.srcKeyName = srcKeyName;
+          this.dstBucketName = dstBucketName;
+          this.dstKeyName = dstKeyName;
+
+      }
+
+      public ParsePDFRequest() {
+      }
+  }
     public static class ParsePDFResponse {
         String filePath;
 
@@ -64,8 +83,10 @@ public class ParsePDF {
         final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
 
         String jarpath = "lib/tabula-1.0.3-jar-with-dependencies.jar";
-        String srcBucketName = event.getBucketName();
-        String srcKeyName = event.getKeyName();
+        String srcBucketName = event.getSrcBucketName();
+        String srcKeyName = event.getSrcKeyName();
+        String dstBucketName = event.getDstBucketName();
+        String dstKeyName = event.getDstKeyName();
         String srcLclFilePath = "/tmp/input.pdf";
         String dstLclFilePath = "/tmp/output.csv";
         String convertComm = String.format(
@@ -105,9 +126,18 @@ public class ParsePDF {
             lsP.waitFor();
             System.out.println ("exit: " + lsP.exitValue());
             lsP.destroy();
+
+            // Upload convrted file to s3
+            PutObjectRequest request = new PutObjectRequest(
+              dstBucketName, dstKeyName, new File(dstLclFilePath));
+            // ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setContentType("plain/text");
+            // metadata.addUserMetadata("x-amz-meta-title", "someTitle");
+            // request.setMetadata(metadata);
+            s3.putObject(request);
         } catch (Exception e) {
             System.out.println ("Encountered exception: " + e);
         }
-        return new ParsePDFResponse(event.getKeyName());
+        return new ParsePDFResponse(event.getDstKeyName());
     }
 }
