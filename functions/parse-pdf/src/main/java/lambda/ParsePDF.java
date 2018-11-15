0730ae5bf6bed3e7791369cc6fdf327f04c08828
Stan Kudrow <stan.kudrow@vice.com>
2018-11-15 16:58:53 -0500

    get s3 perms working with java lambda

--- functions/parse-pdf/src/main/java/lambda/ParsePDF.java
+++ functions/parse-pdf/src/main/java/lambda/ParsePDF.java
@@ -10,6 +10,9 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import java.io.FileOutputStream;
 import java.io.File;
 
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class ParsePDF {
 
@@ -80,6 +83,21 @@ public class ParsePDF {
             S3Object o = s3.getObject(srcBucketName, srcKeyName);
             S3ObjectInputStream s3is = o.getObjectContent();
             FileOutputStream fos = new FileOutputStream(new File(srcLclFilePath));
+            ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(srcBucketName).withMaxKeys(2);
+            ListObjectsV2Result result;
+
+            do {
+                result = s3.listObjectsV2(req);
+
+                for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+                    System.out.printf(" - %s (size: %d)\n", objectSummary.getKey(), objectSummary.getSize());
+                }
+                // If there are more than maxKeys keys in the bucket, get a continuation token
+                // and list the next objects.
+                String token = result.getNextContinuationToken();
+                System.out.println("Next Continuation Token: " + token);
+                req.setContinuationToken(token);
+            } while (result.isTruncated());
 
             p = Runtime.getRuntime().exec("ls -lah /tmp");
             BufferedReader br = new BufferedReader(
@@ -91,7 +109,9 @@ public class ParsePDF {
             p.waitFor();
             System.out.println ("exit: " + p.exitValue());
             p.destroy();
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            System.out.println ("Encountered exception: " + e);
+        }
         return new ParsePDFResponse(event.getKeyName());
     }
 }
