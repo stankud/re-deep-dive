6d26d442caacd400af61d96655037158932de130
Stan Kudrow <stan.kudrow@vice.com>
2018-11-28 17:51:22 -0500

    add router lambda staging

--- functions/router/routing.py
+++ functions/router/routing.py
@@ -1,4 +1,34 @@
+class Stage:
+    DROPBOX = 'dropbox'
+    PDF_IN = 'pdf-in'
+    PDF_OUT = 'pdf-out'
+    CSV_OUT = 'csv-out'
+
 class Router:
 
-    def route(self, file):
+    def __init__(self, file):
+        self.file = file
+
+    def _dropbox(self):
         pass
+
+    def _pdf_in(self):
+        pass
+
+    def _pdf_out(self):
+        pass
+
+    def _csv_out(self):
+        pass
+
+    def route(self):
+        stage = self.file.params['stage']
+
+        if stage == 'dropbox':
+            self._dropbox()
+        elif stage == 'pdf-in':
+            self._pdf_in()
+        elif stage == 'pdf-out':
+            self._pdf_out()
+        elif stage == 'csv-out':
+            self._csv_out()
