d2be0e022d0b40a5ff98cd183549fbe9703472e6
Stan Kudrow <stan.kudrow@vice.com>
2018-11-28 14:34:42 -0500

    add S3Event

--- functions/router/main.py
+++ functions/router/main.py
@@ -4,19 +4,15 @@ Lambda example with external dependency
 import sys
 sys.path.insert(0, "./lib")
 import logging
-import subprocess
-# import tabula as tab
+from parser.parser import S3Event
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 def handle(event, context):
-    """
-    Lambda handler
-    """
-    logger.info("%s - %s", event, context)
-    output = subprocess.check_output(['python', '-V'])
-    logger.info(output)
-
+    e = S3Event(event)
+    logger.info(e.bucket)
+    logger.info(e.key)
     return event
